dd21e0bada2fa8a79bb5130b5c74a0f2e5ef08dc
Paul Milliken <paul.milliken@gmail.com>
2014-01-21 09:08:38 +1300

    changes name of launch file

--- csl_startup.bash
+++ csl_startup.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 find /home/hauler/.ros/log/* -maxdepth 0 -mtime 0 -exec rm -r {} \;
 sleep 1;
-roslaunch axis_camera paull.launch;
+source /home/hauler/catkin_ws/devel/setup.bash;
+roslaunch axis_camera csl.launch;
 
